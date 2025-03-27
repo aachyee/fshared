97c3a6df80cee0a7e66ec634fd44fbbe9177d88f
aachyee <aachyee@gmail.com>
2025-03-27 16:30:37 +0900

    trying fix install error
    deno  install --global --unstable-node-globals --allow-all -n fshare https://deno.land/x/fshare/main.ts
    error: Relative import path "$std/path/mod.ts" not prefixed with / or ./ or ../ and not in import map from "https://deno.land/x/fshare@v0.2.0/deps.ts" at https://deno.land/x/fshare@v0.2.0/deps.ts:2:32

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
-export { parse as parseFlags } from "$std/flags/mod.ts";
-export { basename, join } from "$std/path/mod.ts";
-export { default as ProgressBar } from "https://deno.land/x/progress@v1.2.8/mod.ts";
+export { parse as parseFlags } from "@std/flags/mod.ts";
+//export { parse as parseFlags } from "@std/cli/parse-args/mod.ts"; // deno add jsr:@std/cli
+export { basename, join } from "@std/path/mod.ts"; // deno add jsr:@std/path
+export { default as ProgressBar } from "https://deno.land/x/progress@v1.5.1/mod.ts";
