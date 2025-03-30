b06ad386a80eaccd5372a3f38bf312c7c27553cf
aachyee <14755633+aachyee@users.noreply.github.com>
2025-03-30 17:01:53 +0900

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
-export { parse as parseFlags } from "$std/flags/mod.ts";
-export { basename, join } from "$std/path/mod.ts";
-export { default as ProgressBar } from "https://deno.land/x/progress@v1.2.8/mod.ts";
+export { parse as parseFlags } from "jsr:@std/flags@0.224.0";
+//export { parseArgs as parseFlags } from "jsr:@std/cli@1.0.15/parse-args";
+export { basename, join } from "jsr:@std/path@1.0.8";
+export { default as ProgressBar } from "jsr:@deno-library/progress@1.5.1";
